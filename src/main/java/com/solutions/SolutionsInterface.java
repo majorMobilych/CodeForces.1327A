fb71afcaf29c9f3259af20f689afb218610c45c9
majorMobilych <godmihovelcplus@gmail.com>
2020-04-02 08:37:51 +0300

    add readMe

--- src/main/java/com/solutions/SolutionsInterface.java
+++ src/main/java/com/solutions/SolutionsInterface.java
@@ -5,6 +5,7 @@ public interface SolutionsInterface {
     /**
      * @param number - checking number;
      * @param amountOfTerms - amount of odd terms summing up;
+     * @return "YES" or "NO", depending on result
      */
     String Solution(int number, int amountOfTerms);
 }
